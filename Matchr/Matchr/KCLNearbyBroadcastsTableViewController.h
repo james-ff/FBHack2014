b01035fa255a14415300242ca52fb5698a1a5c54
James Bellamy <k1209605@kcl.ac.uk>
2014-03-15 17:32:33 +0000

    Tableview added and labels edited

---
+++ Matchr/Matchr/KCLNearbyBroadcastsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KCLNearbyBroadcastsTableViewController.h
+//  Matchr
+//
+//  Created by James Bellamy on 15/03/2014.
+//  Copyright (c) 2014 KCL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KCLNearbyBroadcastsTableViewController : UITableViewController
+
+@end
