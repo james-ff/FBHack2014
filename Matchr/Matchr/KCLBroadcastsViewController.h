4e7ef7c545ed302196dd7d2d657ecb10b33ef47f
Hani Kazmi <hanikazmi@me.com>
2014-03-15 17:14:41 +0000

    Add KCLBroadcastViewController

---
+++ Matchr/Matchr/KCLBroadcastsViewController.h
@@ -0,0 +1,13 @@
+//
+//  KCLBroadcastsViewController.h
+//  Matchr
+//
+//  Created by Hani Kazmi on 15/03/2014.
+//  Copyright (c) 2014 KCL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KCLBroadcastsViewController : UIViewController
+
+@end
